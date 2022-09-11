307de8dc614f8047e6113deb60ffeacb9eddfa47
darkside72fx <hoangtuathat@gmail.com>
2022-09-11 14:19:19 +0700

    first

---
+++ src/main/java/com/example/hmusic24h/Hmusic24HApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmusic24h;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hmusic24HApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hmusic24HApplication.class, args);
+    }
+
+}
