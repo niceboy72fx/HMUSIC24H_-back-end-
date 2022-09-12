53bb802347f6aa95e51f22dc954093864c8c8fd6
LQKAn <lequykhangan@gmail.com>
2022-09-12 09:45:52 +0700

    test

---
+++ src/test/java/com/example/hmusic24h/Hmusic24HApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hmusic24h;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hmusic24HApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
